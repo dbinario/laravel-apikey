79c390b0cb2ccee068ed711be661da55a14913b7
Fernando Ruiz <fruiz@wellcom.com.mx>
2022-01-04 09:55:51 -0600

    se crea migracion y modelo de api key

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function apikeys()
+    {
+        return $this->hasMany(ApiKey::class);
+    }
+
 }
